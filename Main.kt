0a25327bbe3601fb5c6410a487b363ba35db5bcf
l000ps <toporoleksandr@gmail.com>
2023-05-28 21:00:49 +0300

    Stage 2/5: Flexible mines

--- Main.kt
+++ Main.kt
@@ -1,15 +1,32 @@
 package minesweeper
 
+var field = Array(9) { CharArray(9) { '.' } }
+
 fun main() {
-    print("""
-        .X.......
-        .....X..X
-        ....X....
-        ......X..
-        ..X......
-        ....X....
-        ..X......
-        ..X......
-        ......X..
-    """.trimIndent())
+    initField()
+    printField()
+}
+
+fun initField() {
+    print("How many mines do you want on the field? ")
+    val mines = readln().toInt()
+    val selectedNumbers = mutableListOf<Int>()
+    while (selectedNumbers.size < mines) {
+        val randomNumber = (1..81).random()
+        if (!selectedNumbers.contains(randomNumber)) {
+            selectedNumbers.add(randomNumber)
+        }
+    }
+    for (i in selectedNumbers) {
+        field[(i - 1) / 9][(i - 1) % 9] = 'X'
+    }
+}
+
+fun printField() {
+    for (i in field) {
+        for (y in i) {
+            print(y)
+        }
+        println()
+    }
 }
